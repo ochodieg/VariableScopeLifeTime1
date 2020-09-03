c7d5c15c768e196632e721213ce0fa9f17ce0946
Ivan <the_real_iori_orochi@hotmail.com>
2020-09-03 08:24:05 -0600

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VariableScopeLifeTime")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VariableScopeLifeTime")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ac9bb5e5-0784-4e2c-a8c6-30de24a5de37")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ VariableScopeLifetime.vb
@@ -0,0 +1,25 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+
+Module VariableScopeLifetime
+
+    Dim y As Integer
+
+    Sub Main()
+        Dim x As Integer
+        Console.WriteLine(x)
+
+        Console.ReadLine()
+
+        y = 4
+    End Sub
+
+    Sub TestX()
+
+        X = 5
+        y = 71
+
+    End Sub
+
+End Module
